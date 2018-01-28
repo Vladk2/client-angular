f2a07e6f66cdaa7bdc42beca3b4f39577717f975
Vladk2 <vladislavk2@yahoo.com>
2018-01-28 07:55:10 +0100

    parl, problem and tenant services updated

--- src/app/services/problem-service/problem.service.spec.ts
+++ src/app/services/problem-service/problem.service.spec.ts
@@ -1,7 +1,10 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { Problem } from '../../models/problem/problem.model';
+import { User } from '../../models/user/user.model';
+import { Comment } from '../../models/problem/comment.model';
+import { DateTime } from '../../models/date/datetime.model';
 import { ProblemService } from './problem.service';
 
 describe('ProblemService', () => {
@@ -25,4 +28,126 @@ describe('ProblemService', () => {
   it('should be created', () => {
     expect(true).toBe(true);
   });
+
+  it('should return list of active problems', () => {
+    const problems = [
+      {'id': 1, 'title': 'Kvar1', 'description': 'opis1', 'active': true, 'imgNo': 0, 'repairDate': '14/12/2018 12:44',
+      'openForAll': false, 'images': null, 'postDate': '14/12/2017 12:44', 'tenant': null, 'firm': null},
+      {'id': 2, 'title': 'Kvar2', 'description': 'opis2', 'active': true, 'imgNo': 0, 'repairDate': '14/12/2018 12:44',
+      'openForAll': true, 'images': null, 'postDate': '14/12/2017 12:44', 'tenant': null, 'firm': null}
+    ];
+
+    service.getProblems().subscribe((res: any) => {
+      expect(res).not.toBe(null);
+      expect(res.length).toBe(2);
+      expect(res[0].description).toBe('opis1');
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/api/problems/');
+
+    expect(request.request.method).toBe('GET');
+
+    request.flush(problems);
+  });
+
+  it('should report a problem successfully', () => {
+
+    const tenantId = 1;
+    const problem = new Problem();
+    problem.title = 'Golem kvar';
+    problem.description = 'Vrlo golem kvar';
+    problem.active = true;
+    problem.openForAll = true;
+
+    service.postProblem(tenantId, problem).subscribe(res => {
+      expect(res).not.toBe(null);
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/api/problems/' + tenantId);
+
+    expect(request.request.method).toBe('POST');
+
+  });
+
+  it('should get all active comments', () => {
+    const user = new User();
+    const problem = new Problem();
+    const comments = [
+      {'id': 1, 'message': 'Komentar1', 'creator': user, 'problem': problem, 'date': '26/12/2017 14:29'},
+      {'id': 2, 'message': 'Komentar2', 'creator': user, 'problem': problem, 'date': '28/12/2017 14:29'}
+    ];
+
+    service.getComments().subscribe((res: any) => {
+      expect(res).not.toBe(null);
+      expect(res.length).toBe(2);
+      expect(res[1].message).toBe('Komentar2');
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/api/problems/comments');
+
+    expect(request.request.method).toBe('GET');
+
+    request.flush(comments);
+  });
+
+  it('should post a comment successfully', () => {
+
+    const user = new User();
+    const problemId = 1;
+    const comment = new Comment();
+    comment.message = 'Komentar na kvar';
+    comment.creator = user;
+
+    service.postComment(problemId, comment).subscribe(res => {
+      expect(res).not.toBe(null);
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/api/problems/comment/' + problemId);
+
+    expect(request.request.method).toBe('POST');
+
+  });
+
+  it('should set a repairDate', () => {
+
+    const problemId = 1;
+    const repairDate = new DateTime('28/12/2017 14:29');
+    service.setRepairDate(problemId, repairDate).subscribe(res => {
+      expect(res).not.toBe(null);
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/api/problems/setRepairDate/' + problemId);
+
+    expect(request.request.method).toBe('PATCH');
+
+  });
+
+
+  it('should forward a problem', () => {
+
+    const problemId = 4;
+    const firmId = 1;
+    service.forwardProblem(problemId, firmId).subscribe(res => {
+      expect(res).not.toBe(null);
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/api/problems/forward/' + problemId + '/' + firmId);
+
+    expect(request.request.method).toBe('PATCH');
+
+  });
+
+  it('should resolve a problem', () => {
+
+    const problemId = 4;
+    service.resolveProblem(problemId).subscribe(res => {
+      expect(res).not.toBe(null);
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/api/problems/solve/' + problemId);
+
+    expect(request.request.method).toBe('PATCH');
+
+  });
+
 });
--- src/app/services/tenant-service/tenant.service.spec.ts
+++ src/app/services/tenant-service/tenant.service.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { TenantService } from './tenant.service';
+import { Announcement } from '../../models/announcement/announcement.model';
 
 describe('TenantService', () => {
 
@@ -25,5 +26,56 @@ describe('TenantService', () => {
   it('should be created', () => {
     expect(true).toBe(true);
   });
+
+  it('should post an announcement', () => {
+
+    const tenantId = 1;
+    const ann = new Announcement();
+    ann.title = 'Naslov obavestenja';
+    ann.message = 'Pouka';
+    service.postAnnouncement(tenantId, ann).subscribe(res => {
+      expect(res).not.toBe(null);
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/api/tenants/announcement/' + tenantId);
+
+    expect(request.request.method).toBe('POST');
+  });
+
+  it('should register new abode', () => {
+
+    const tenantId = 1;
+    const abode = {'builId': 2, 'apartmentNo': 12};
+    service.createNewTenant(abode).subscribe(res => {
+      expect(res).not.toBe(null);
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/api/tenants/');
+
+    expect(request.request.method).toBe('POST');
+
+  });
+
+
+  it('should get all annoucements for a logged-in tenant', () => {
+    const anns = [];
+    const ann = new Announcement();
+    ann.title = 'Naslov obavestenja';
+    ann.message = 'Pouka';
+    const tenantId = 2;
+    anns.push(ann);
+    service.getAnnouncements(tenantId).subscribe((res: any) => {
+      expect(res).not.toBe(null);
+      expect(res.length).toBe(1);
+      expect(res[0].message).toBe('Pouka');
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/api/tenants/announcement/' + tenantId);
+
+    expect(request.request.method).toBe('GET');
+
+    request.flush(anns);
+  });
+
 
 });
