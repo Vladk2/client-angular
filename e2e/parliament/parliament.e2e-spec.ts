ed43cd838e9bc68dd5a857b28e5094638c74b12c
Novica Nikolic <nole0223@gmail.com>
2018-01-29 14:35:30 +0100

    comit

--- e2e/parliament/parliament.e2e-spec.ts
+++ e2e/parliament/parliament.e2e-spec.ts
@@ -14,14 +14,14 @@ describe('Parliament Page', () => {
 
   it('should display correct navbar title', () => {
 
-    loginPage = new LoginPage();
-    loginPage.navigateTo();
-    const usernameInput = loginPage.getUsernameInput();
-    const passwordInput = loginPage.getPasswordInput();
-    const submitButton = loginPage.getSubmitButton();
-    usernameInput.sendKeys('riggy');
-    passwordInput.sendKeys('123');
-    submitButton.click();
+    //loginPage = new LoginPage();
+    //loginPage.navigateTo();
+    //const usernameInput = loginPage.getUsernameInput();
+    //const passwordInput = loginPage.getPasswordInput();
+    //const submitButton = loginPage.getSubmitButton();
+    //usernameInput.sendKeys('nn');
+    //passwordInput.sendKeys('123');
+    //submitButton.click();
 
 
     page.navigateTo();
